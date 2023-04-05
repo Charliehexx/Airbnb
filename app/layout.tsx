7ad290095c16a5d8be628173880394496195e658
Aditya <adi2652002@gmail.com>
2023-04-05 12:45:13 +0530

    Remaining Project Completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,16 @@
 import { Nunito } from 'next/font/google'
 
 import Navbar from '@/app/components/navbar/Navbar';
-
+import LoginModal from '@/app/components/modals/LoginModal';
 import RegisterModal from '@/app/components/modals/RegisterModal';
-
-
+import SearchModal from '@/app/components/modals/SearchModal';
+import RentModal from '@/app/components/modals/RentModal';
 
 import ToasterProvider from '@/app/providers/ToasterProvider';
 
 import './globals.css'
 
 import getCurrentUser from './actions/getCurrentUser';
-import LoginModal from './components/modals/LoginModal';
-import RentModal from './components/modals/RentModal';
-import SearchModal from './components/modals/SearchModal';
 
 export const metadata = {
   title: 'Airbnb',
@@ -34,7 +31,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        
+      
           <ToasterProvider />
           <LoginModal />
           <RegisterModal />
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,56 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Container from "@/app/components/Container";
+import ListingCard from "@/app/components/listings/ListingCard";
+import EmptyState from "@/app/components/EmptyState";
+
+import getListings, { 
+  IListingsParams
+} from "@/app/actions/getListings";
+import getCurrentUser from "@/app/actions/getCurrentUser";
 
 
-const inter = Inter({ subsets: ['latin'] })
+interface HomeProps {
+  searchParams: IListingsParams
+};
 
-export default function Home() {
+const Home = async ({ searchParams }: HomeProps) => {
+  const listings = await getListings(searchParams);
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+    
+        <EmptyState showReset />
+    
+    );
+  }
+
   return (
-    <main className='text-rose-500'>
-
-    </main>
+    
+      <Container>
+        <div 
+          className="
+            pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          "
+        >
+          {listings.map((listing: any) => (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
+          ))}
+        </div>
+      </Container>
+    
   )
 }
+
+export default Home;
